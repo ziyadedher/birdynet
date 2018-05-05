4e3b2a9f067d1c610646cd21a2fcb0e1f57b4702
Ziyad Edher <ziyad.edher@gmail.com>
2018-05-05 17:12:36 -0400

    Implement word strategy

--- src/game/strategy.py
+++ src/game/strategy.py
@@ -1,5 +1,10 @@
 """This module contains strategies used to play the game."""
 
+import random
+import pygame
+
+from game import config
+
 
 class Strategy:
     """Defines an abstract strategy that can be built on."""
@@ -45,3 +50,29 @@ class InputStrategy(Strategy):
     def get_move(self) -> bool:
         """Return whether or not to move the player."""
         return self._key_down_this_frame
+
+
+class WordStrategy(Strategy):
+    """Defines a strategy to move depending on word."""
+
+    _current_word: str
+
+    def __init__(self) -> None:
+        """Initialize this word strategy."""
+        super().__init__()
+        self._current_word = ""
+        self.new_word()
+
+    def new_word(self) -> None:
+        """Assign a new word for the strategy."""
+        self._current_word = random.choice(config.WORDS).lower()
+        print(self._current_word)
+
+    def get_move(self) -> bool:
+        """Return whether or not to move the player."""
+        if pygame.key.get_pressed()[ord(self._current_word[0])]:
+            self._current_word = self._current_word[1:]
+            if not self._current_word:
+                self.new_word()
+            return True
+        return False
