9a364d6e9dec8417dedc4f7a4d864c33a4f80f4a
Ziyad Edher <ziyad.edher@gmail.com>
2018-05-07 09:51:07 -0400

    Recognize folders as modules

--- src/start.py
+++ src/start.py
@@ -6,7 +6,7 @@ from gui.interactive import InteractiveGUI
 
 if __name__ == '__main__':
     GAME = Game()
-    GAME.player.attach_strategy(strategy.WordStrategy())
+    GAME.player.attach_strategy(strategy.InputStrategy())
 
     GUI = InteractiveGUI(GAME)
 
